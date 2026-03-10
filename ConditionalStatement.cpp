038947442cb59c5597de63d1c7776e16d9678e30
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-10 21:31:46 +0800

    Membuat rumus untuk sebuah penghitungan bmi

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -8,3 +8,8 @@ void Input(float &berat, float &tinggi)
     cout << "Masukkan Tinggi Badan (m) : "; 
     cin >> tinggi;
 }
+
+float RumusBMI(float berat, float tinggi){
+    return berat / (tinggi * tinggi);
+}
+
