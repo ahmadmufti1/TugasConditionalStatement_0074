c98066b172277bf7f177f0a0d7d3519ac99a7787
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-10 21:30:00 +0800

    Mendeklarasi fungsi void input untuk memasukkan data

---
+++ ConditionalStatement.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void Input(float &berat, float &tinggi)
+{
+    cout << "Masukkan Berat Badan (kg) : ";
+    cin >> berat;
+    cout << "Masukkan Tinggi Badan (m) : "; 
+    cin >> tinggi;
+}
