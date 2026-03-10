69134af8b2a4307751596014f08a1da1dbc21c35
ahmadmufti1 <muftiahmad617@gmail.com>
2026-03-10 21:33:31 +0800

    Membuat sebuah pengkondisian

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -13,3 +13,14 @@ float RumusBMI(float berat, float tinggi){
     return berat / (tinggi * tinggi);
 }
 
+string BeratBadan(float berat){
+    if (berat < 18.5) {
+        return "Berat Badan Kurus";
+    } else if (berat >= 18.5 && berat < 25) {
+        return "Berat Badan Normal";
+    } else if (berat >= 25 && berat < 30) {
+        return "Berat Badan Gemuk";
+    } else {
+        return "Obesitas";
+    }
+}
